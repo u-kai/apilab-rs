e8b6446e69bbb0dd02fec4035c6cd670c0f27cff
u-kai <76635578+u-kai@users.noreply.github.com>
2022-08-28 09:42:44 +0900

    clear base64 case hello world

--- apis/utils/src/base64.rs
+++ apis/utils/src/base64.rs
@@ -2,9 +2,10 @@ use std::collections::HashMap;
 
 #[cfg(test)]
 pub fn encode<T: AsRef<str>>(source: T) -> String {
-    let bytes = source.as_ref().bytes();
-    //bytes
-    "SGVsbG8sV29ybGQh".to_string()
+    let source = source.as_ref();
+    let map = Base64BitMap::new();
+    let mut iter = Base64BitStreamIter::new(source);
+    iter.encode(map)
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -21,14 +22,121 @@ enum Bit {
     Zero,
     One,
 }
+struct BitIter {
+    bits: Vec<Bit>,
+}
+impl BitIter {
+    fn new(source: &str) -> Self {
+        let bits =
+            source
+                .bytes()
+                .map(|byte| Bit::from_byte(byte))
+                .fold(Vec::new(), |mut acc, mut cur| {
+                    for c in cur {
+                        acc.push(c);
+                    }
+                    acc
+                });
+        Self { bits }
+    }
+}
+impl Iterator for BitIter {
+    type Item = Bit;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.bits.len() == 0 {
+            return None;
+        };
+        Some(self.bits.remove(0))
+    }
+}
+impl Bit {
+    fn str_to_bit_iter(source: &str) -> BitIter {
+        BitIter::new(source)
+    }
+    fn from_byte(byte: u8) -> [Bit; 8] {
+        let mut bits = [Bit::Zero; 8];
+        (0..8).for_each(|i| {
+            let bit = (byte >> (7 - i)) & 1;
+            match bit {
+                0 => bits[i] = Bit::Zero,
+                1 => bits[i] = Bit::One,
+                _ => panic!("not 1 or 0 {}", bit),
+            }
+        });
+        bits
+    }
+}
 mod base64_tests {
     use super::*;
+    #[test]
+    fn from_byte_test() {
+        assert_eq!(Bit::from_byte(0), [Bit::Zero; 8]);
+        assert_eq!(
+            Bit::from_byte(1),
+            [
+                Bit::Zero,
+                Bit::Zero,
+                Bit::Zero,
+                Bit::Zero,
+                Bit::Zero,
+                Bit::Zero,
+                Bit::Zero,
+                Bit::One,
+            ]
+        );
+        assert_eq!(
+            Bit::from_byte(254),
+            [
+                Bit::One,
+                Bit::One,
+                Bit::One,
+                Bit::One,
+                Bit::One,
+                Bit::One,
+                Bit::One,
+                Bit::Zero,
+            ]
+        );
+    }
     #[test]
     fn base64_test() {
         let source = "Hello,World!";
         let encode = encode(source);
         assert_eq!(encode, "SGVsbG8sV29ybGQh".to_string());
     }
+}
+struct Base64BitStreamIter {
+    bit_iter: BitIter,
+}
+impl Base64BitStreamIter {
+    fn new(source: &str) -> Self {
+        Self {
+            bit_iter: Bit::str_to_bit_iter(source),
+        }
+    }
+    fn encode(&mut self, map: Base64BitMap) -> String {
+        let mut result = String::new();
+        let mut base64 = self.pop_base64_bit();
+        while base64.is_some() {
+            let key = base64.unwrap();
+            let value = map.0.get(&key).unwrap();
+            result.push(*value);
+            base64 = self.pop_base64_bit();
+        }
+        result
+    }
+    fn pop_base64_bit(&mut self) -> Option<Base64BitStream> {
+        let mut stream = [Bit::Zero; 6];
+        for i in 0..6 {
+            let bit = self.bit_iter.next();
+            if bit.is_none() {
+                return None;
+            }
+            stream[i] = bit.unwrap().clone()
+        }
+        Some(Base64BitStream { stream })
+    }
+    //fn
 }
 struct Base64BitMap(HashMap<Base64BitStream, char>);
 impl Base64BitMap {
