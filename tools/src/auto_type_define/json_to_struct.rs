a0d84911b9a07385633474ad70e6d9c0baf65d69
u-kai <76635578+u-kai@users.noreply.github.com>
2022-09-04 17:38:55 +0900

    clear set requires

--- tools/src/auto_type_define/json_to_struct.rs
+++ tools/src/auto_type_define/json_to_struct.rs
@@ -1,32 +1,94 @@
+use std::{
+    fs::File,
+    io::{BufWriter, Write},
+    path::Path,
+};
+
 use serde_json::{Map, Result, Value};
 
+#[derive(Debug, Clone)]
+struct RequireFiled {
+    struct_name: String,
+    filed_name: String,
+}
+impl RequireFiled {
+    fn new(struct_name: impl Into<String>, filed_name: impl Into<String>) -> Self {
+        Self {
+            struct_name: struct_name.into(),
+            filed_name: filed_name.into(),
+        }
+    }
+    fn is_require(&self, struct_name: &str, filed_name: &str) -> bool {
+        self.struct_name == struct_name && self.filed_name == filed_name
+    }
+}
 pub struct JsonStructBuilder {
     derive: String,
     struct_name: String,
+    require_fileds: Vec<RequireFiled>,
+    pub_fileds: Option<Vec<String>>,
 }
+
 impl JsonStructBuilder {
-    pub fn new(derives: Vec<&str>, struct_name: impl Into<String>) -> Self {
+    pub fn new(struct_name: impl Into<String>) -> Self {
         Self {
-            derive: derives.join(",").to_string(),
+            derive: "Serialize,Desrialize".to_string(),
             struct_name: struct_name.into(),
+            require_fileds: Vec::new(),
+            pub_fileds: None,
         }
     }
-    fn from_derive(derive: impl Into<String>, struct_name: impl Into<String>) -> Self {
+    fn inherit_option_fileds(
+        &self,
+        derive: impl Into<String>,
+        struct_name: impl Into<String>,
+    ) -> Self {
         Self {
             derive: derive.into(),
             struct_name: struct_name.into(),
+            require_fileds: self.require_fileds.clone(),
+            pub_fileds: self.pub_fileds.clone(),
         }
+    }
+    pub fn new_with_drives(derives: Vec<&str>, struct_name: impl Into<String>) -> Self {
+        Self {
+            derive: derives.join(",").to_string(),
+            struct_name: struct_name.into(),
+            require_fileds: Vec::new(),
+            pub_fileds: None,
+        }
+    }
+    pub fn set_require(
+        &mut self,
+        struct_name: impl Into<String>,
+        filed_name: impl Into<String>,
+    ) -> &mut Self {
+        self.require_fileds
+            .push(RequireFiled::new(struct_name, filed_name));
+        self
+    }
+    pub fn from_json_example_to_file(
+        &self,
+        source: &str,
+        file_path: impl AsRef<Path>,
+    ) -> Result<()> {
+        let string = self.from_json_example(source)?;
+        let buf = string.as_bytes();
+        let file = File::create(file_path).unwrap();
+        let mut writer = BufWriter::new(file);
+        let _ = writer.write_all(buf);
+        Ok(())
     }
     pub fn from_json_example(&self, source: &str) -> Result<String> {
         let json_value = Self::json_to_value(source)?;
         let mut child_buffer = Vec::new();
         let s = match json_value {
             Value::Object(object) => self.case_object(&object, &mut child_buffer),
-            Value::String(_) => self.case_string(),
+            Value::String(_) => self.case_string(None),
             Value::Array(array) => self.case_array_with_key("", &array, &mut child_buffer),
             Value::Null => self.case_null(),
-            Value::Bool(_) => self.case_bool(),
-            Value::Number(_) => self.case_number(),
+            Value::Bool(_) => self.case_bool(None),
+            Value::Number(_) => self.case_number(None),
         };
         let s = child_buffer
             .iter()
@@ -34,25 +96,33 @@ impl JsonStructBuilder {
             .fold(s, |acc, cur| format!("{}\n{}", acc, cur));
         Ok(s)
     }
+    fn is_require(&self, filed_name: &str) -> bool {
+        self.require_fileds
+            .iter()
+            .any(|req| req.is_require(&self.struct_name, filed_name))
+    }
     fn case_object(&self, object: &Map<String, Value>, child_buffer: &mut Vec<String>) -> String {
         let mut object_string = self.struct_statement();
         for key in object.keys() {
-            println!("key {}", key);
             let child_object = object.get(key).unwrap();
             let child_object_value = match child_object {
                 Value::Object(object) => {
                     let child_struct_name = self.key_to_struct_name(key);
-                    println!("child_struct_name  {}", child_struct_name);
-                    let child_builder = Self::from_derive(&self.derive, &child_struct_name);
+                    let child_builder =
+                        self.inherit_option_fileds(&self.derive, &child_struct_name);
                     let child_string = child_builder.case_object(object, child_buffer);
                     child_buffer.push(child_string);
-                    format!("Option<{}>", child_struct_name)
+                    if self.is_require(key) {
+                        child_struct_name
+                    } else {
+                        format!("Option<{}>", child_struct_name)
+                    }
                 }
                 Value::Array(array) => self.case_array_with_key(key, array, child_buffer),
-                Value::String(_) => self.case_string(),
+                Value::String(_) => self.case_string(Some(key)),
                 Value::Null => self.case_null(),
-                Value::Bool(_) => self.case_bool(),
-                Value::Number(_) => self.case_number(),
+                Value::Bool(_) => self.case_bool(Some(key)),
+                Value::Number(_) => self.case_number(Some(key)),
             };
             object_string = format!(
                 "{}{}: {}{}",
@@ -85,31 +155,71 @@ impl JsonStructBuilder {
         match represent {
             Value::Object(object) => {
                 let struct_name = self.key_to_struct_name(key);
-                let builder = Self::from_derive(&self.derive, &struct_name);
+                let builder = self.inherit_option_fileds(&self.derive, &struct_name);
                 let string = builder.case_object(object, child_buffer);
                 child_buffer.push(string);
-                format!("Option<Vec<{}>>", struct_name)
+                if self.is_require(key) {
+                    format!("Vec<{}>", struct_name)
+                } else {
+                    format!("Option<Vec<{}>>", struct_name)
+                }
             }
             Value::Array(array) => {
                 self.case_array_with_key(&format!("Vec<{}>", key), array, child_buffer)
             }
-            Value::Null => String::new(),
-            Value::Bool(_) => String::from("Option<Vec<bool>>"),
-            Value::String(_) => String::from("Option<Vec<String>>"),
-            Value::Number(_) => String::from("Option<Vec<f64>>"),
+            Value::Null => self.case_null(),
+            Value::Bool(_) => {
+                if self.is_require(key) {
+                    format!("Vec<bool>")
+                } else {
+                    String::from("Option<Vec<bool>>")
+                }
+            }
+            Value::String(_) => {
+                if self.is_require(key) {
+                    format!("Vec<String>")
+                } else {
+                    String::from("Option<Vec<String>>")
+                }
+            }
+            Value::Number(_) => {
+                if self.is_require(key) {
+                    format!("Vec<f64>")
+                } else {
+                    String::from("Option<Vec<f64>>")
+                }
+            }
         }
     }
     fn case_null(&self) -> String {
         String::new()
     }
-    fn case_bool(&self) -> String {
-        String::from("Option<bool>")
+    fn case_bool(&self, key: Option<&str>) -> String {
+        match key {
+            Some(key) if self.is_require(key) => {
+                format!("bool")
+            }
+            _ => String::from("Option<bool>"),
+        }
     }
-    fn case_number(&self) -> String {
-        String::from("Option<f64>")
+    fn case_number(&self, key: Option<&str>) -> String {
+        println!("key {:?} ", key);
+        println!("self.is {:?} ", &self.struct_name);
+        println!("self.fileds {:?} ", &self.require_fileds);
+        match key {
+            Some(key) if self.is_require(key) => {
+                format!("f64")
+            }
+            _ => String::from("Option<f64>"),
+        }
     }
-    fn case_string(&self) -> String {
-        String::from("Option<String>")
+    fn case_string(&self, key: Option<&str>) -> String {
+        match key {
+            Some(key) if self.is_require(key) => {
+                format!("String")
+            }
+            _ => String::from("Option<String>"),
+        }
     }
     fn json_to_value(source: &str) -> Result<Value> {
         let json: Value = serde_json::from_str(source)?;
@@ -142,6 +252,44 @@ mod json_define_to_struct {
     use super::*;
     const FIELD_SPACE: &str = "\n    ";
     #[test]
+    fn test_add_require() {
+        let complicated_json = r#"
+            {
+                "data":[
+                    {
+                        "id":12345,
+                        "test":"test-string",
+                        "entities":{
+                            "id":0
+                        }
+                    }
+                ]
+            }
+        "#;
+        let struct_name = "TestJson";
+        let tobe = r#"#[derive(Serialize,Desrialize)]
+struct TestJson {
+    data: Vec<TestJsonData>,
+}
+#[derive(Serialize,Desrialize)]
+struct TestJsonData {
+    entities: Option<TestJsonDataEntities>,
+    id: f64,
+    test: Option<String>,
+}
+#[derive(Serialize,Desrialize)]
+struct TestJsonDataEntities {
+    id: Option<f64>,
+}"#
+        .to_string();
+        let mut builder =
+            JsonStructBuilder::new_with_drives(vec!["Serialize", "Desrialize"], struct_name);
+        builder
+            .set_require("TestJson", "data")
+            .set_require("TestJsonData", "id");
+        assert_eq!(builder.from_json_example(complicated_json).unwrap(), tobe);
+    }
+    #[test]
     fn test_from_flat_json_example() {
         let flat_json = r#"
             {
@@ -151,7 +299,8 @@ mod json_define_to_struct {
         "#;
         let struct_name = "TestJson";
         let tobe = format!("#[derive(Serialize,Desrialize)]\nstruct {} {{{}id: Option<f64>,{}test: Option<String>,\n}}",struct_name,FIELD_SPACE,FIELD_SPACE);
-        let builder = JsonStructBuilder::new(vec!["Serialize", "Desrialize"], struct_name);
+        let builder =
+            JsonStructBuilder::new_with_drives(vec!["Serialize", "Desrialize"], struct_name);
         assert_eq!(builder.from_json_example(flat_json).unwrap(), tobe);
     }
     #[test]
@@ -185,7 +334,8 @@ struct TestJsonDataEntities {
     id: Option<f64>,
 }"#
         .to_string();
-        let builder = JsonStructBuilder::new(vec!["Serialize", "Desrialize"], struct_name);
+        let builder =
+            JsonStructBuilder::new_with_drives(vec!["Serialize", "Desrialize"], struct_name);
         assert_eq!(builder.from_json_example(complicated_json).unwrap(), tobe);
     }
     #[test]
@@ -219,7 +369,8 @@ struct TestJsonDataEntities {
     id: Option<f64>,
 }"#
         .to_string();
-        let builder = JsonStructBuilder::new(vec!["Serialize", "Desrialize"], struct_name);
+        let builder =
+            JsonStructBuilder::new_with_drives(vec!["Serialize", "Desrialize"], struct_name);
         assert_eq!(builder.from_json_example(complicated_json).unwrap(), tobe);
     }
 }
